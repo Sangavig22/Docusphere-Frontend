5e7fe7cc1219ac269f6d924ce6c03e0a4bf2d4b5
adshaya30 <yaliads30@gmail.com>
2026-03-08 16:38:10 +0530

    Delete landing page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,7 @@
-import LandingPage from "../pages/LandingPage";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 
 export const routes = [
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
   {
     path: "/signup",
     element: <SignUp />,
