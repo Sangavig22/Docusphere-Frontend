d52f1df82c15cd88e6567cb6a12d27cd723187d4
Nilakshana23 <nilaks2k02@gmail.com>
2026-03-09 18:10:30 +0530

    Edited topbar,sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-
 import './App.css'
+import AppRoutes from "./routes"
+import { DocumentsProvider } from "./hooks/useDocumentsStore";
 
 function App() {
-  const [count, setCount] = useState(0)
-
    return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        Hello World
-      </h1>
-    </div>
+    <DocumentsProvider>
+      <AppRoutes />
+    </DocumentsProvider>
   );
 }
 
