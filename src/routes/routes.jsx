490502a0c72383a53e153608daf1f1f2cb1d61d6
Niroja Vijayakumar <nirovijay.nv@gmail.com>
2026-03-15 17:03:22 +0530

    Update all routes in routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import DashboardLayout from "../components/Layout/DashboardLayout";
 import AdminLayout from "../components/Layout/AdminLayout.jsx";
+
+import Dashboard from "../Pages/Dashboard";
+import UploadPage from "../pages/UploadPage";
 import AdminDashboardPage from "../pages/AdminDashboardPage.jsx";
 
+// Wrapper functions
 function withLayout(element, pageTitle, pageSubtitle) {
+  return (
+    <DashboardLayout pageTitle={pageTitle} pageSubtitle={pageSubtitle}>
+      {element}
+    </DashboardLayout>
+  );
+}
+
+function withAdminLayout(element, pageTitle, pageSubtitle) {
   return (
     <AdminLayout pageTitle={pageTitle} pageSubtitle={pageSubtitle}>
       {element}
@@ -10,11 +23,29 @@ function withLayout(element, pageTitle, pageSubtitle) {
   );
 }
 
+// Combined routes
 export const routes = [
+  // User routes
+  {
+    path: "/dashboard",
+    element: withLayout(<Dashboard />, "Dashboard", "Overview of your workspace"),
+  },
+  {
+    path: "/uploads",
+    element: withLayout(
+      <UploadPage />,
+      "Uploads",
+      "Drag and drop files or browse to upload."
+    ),
+  },
+
+  // Admin route
   {
     path: "/admin/dashboard",
-    element: withLayout(<AdminDashboardPage />, 
-      "Dashboard", 
-      "Manage Your platform with ease"),
+    element: withAdminLayout(
+      <AdminDashboardPage />,
+      "Dashboard",
+      "Manage your platform with ease"
+    ),
   },
 ];
