58e34d4883da199d216a9a92c032570534a7cc74
adshaya30 <yaliads30@gmail.com>
2026-03-17 15:56:16 +0530

    Added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
 import AppRoutes from "./routes"
+import { DocumentsProvider } from "./hooks/useDocumentsStore";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <AppRoutes />
+   return (
+    <DocumentsProvider>
+      <ToastContainer 
+        position="top-right" 
+        autoClose={3000} 
+        hideProgressBar={false} 
+        newestOnTop={true} 
+        closeOnClick 
+        rtl={false} 
+        pauseOnFocusLoss 
+        draggable 
+        pauseOnHover 
+      />
+      <AppRoutes />
+    </DocumentsProvider>
   );
 }
 
