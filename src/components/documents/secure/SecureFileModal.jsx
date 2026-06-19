50b41d519a027cb8547bfe2d4bf52b96b1b0b4b6
A.Induwara umayanaga <umayangaai.23@uom.lk>
2026-06-19 11:48:10 +0530

    Update SecureFileModal.jsx

--- src/components/documents/secure/SecureFileModal.jsx
+++ src/components/documents/secure/SecureFileModal.jsx
@@ -456,7 +456,7 @@ export default function SecureFileModal({
                   if (error) setError("");
                 }}
                 disabled={busy}
-                autoFocus 
+                autoFocus
               />
               <PasswordInput
                 id="secure-file-reset-confirm-password"
